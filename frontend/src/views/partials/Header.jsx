005eaaf11f05e68328250904f50b8983a474278a
iamansoni07 <aman.soni0713@gmail.com>
2025-09-01 22:07:23 +0530

    feat: add mobile-responsive branding to Header component

--- frontend/src/views/partials/Header.jsx
+++ frontend/src/views/partials/Header.jsx
@@ -15,6 +15,9 @@ function Header() {
                         <span className="h6 d-none d-sm-inline-block text-white">Menu</span>
                         <span className="navbar-toggler-icon" />
                     </button>
+                    <div className="d-flex d-lg-none align-items-center ms-2">
+                        <span className="navbar-text text-white me-2">FlowPress</span>
+                    </div>
                     <div className="collapse navbar-collapse" id="navbarCollapse">
                         <div className="nav mt-3 mt-lg-0 px-4 flex-nowrap align-items-center">
                             <div className="nav-item w-100">
